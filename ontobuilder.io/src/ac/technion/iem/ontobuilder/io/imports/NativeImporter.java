0d0a0c56a32094a6f15f0e86196d5e4372a92ed5
tomers77 <tomers77@localhost>
2011-09-15 15:17:35 +0300

    Added native importer shell

---
+++ ontobuilder.io/src/ac/technion/iem/ontobuilder/io/imports/NativeImporter.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package ac.technion.iem.ontobuilder.io.imports;
+
+import java.io.File;
+import java.io.IOException;
+
+import ac.technion.iem.ontobuilder.core.ontology.Ontology;
+import ac.technion.iem.ontobuilder.core.utils.files.XmlFileHandler;
+
+/**
+ * @author Tomer Sagi
+ * Importer for Ontobuilder native format. Actually a shell for xmlFileHandler
+ */
+public class NativeImporter implements Importer {
+	
+	XmlFileHandler xfh = new XmlFileHandler();
+	
+	/* (non-Javadoc)
+	 * @see ac.technion.iem.ontobuilder.io.imports.Importer#importFile(java.io.File)
+	 */
+	@Override
+	public Ontology importFile(File file) throws ImportException {
+		Ontology res;
+		try {
+			res = xfh.readOntologyXMLFile(file.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new ImportException(e.getMessage());
+		} 
+		return res;
+	}
+
+}
