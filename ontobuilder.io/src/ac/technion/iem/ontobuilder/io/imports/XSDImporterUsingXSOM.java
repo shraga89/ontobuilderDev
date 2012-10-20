882854c09c6fb5da27b5cac1ca6e2753269c2119
tomers77 <tomers77@localhost>
2012-10-20 20:02:31 +0200

    Cleanup

--- ontobuilder.io/src/ac/technion/iem/ontobuilder/io/imports/XSDImporterUsingXSOM.java
+++ ontobuilder.io/src/ac/technion/iem/ontobuilder/io/imports/XSDImporterUsingXSOM.java
@@ -1,7 +1,6 @@
 package ac.technion.iem.ontobuilder.io.imports;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,15 +9,12 @@ import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import ac.technion.iem.ontobuilder.core.ontology.Domain;
 import ac.technion.iem.ontobuilder.core.ontology.DomainEntry;
@@ -27,6 +23,7 @@ import ac.technion.iem.ontobuilder.core.ontology.OntologyClass;
 import ac.technion.iem.ontobuilder.core.ontology.Relationship;
 import ac.technion.iem.ontobuilder.core.ontology.Term;
 import ac.technion.iem.ontobuilder.io.utils.xml.XSDEntityResolver;
+
 import com.sun.xml.xsom.XSComplexType;
 import com.sun.xml.xsom.XSElementDecl;
 import com.sun.xml.xsom.XSModelGroup;
