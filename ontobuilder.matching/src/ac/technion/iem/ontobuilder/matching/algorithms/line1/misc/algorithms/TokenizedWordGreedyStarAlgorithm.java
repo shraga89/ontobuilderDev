a3b828cdf470ed52e9df4188c774deb965da5bc2
Sapir Golan <sapirgolan@gmail.com>
2013-08-03 19:01:12 +0300

    Removed dead code (was with comments) and added test - TokenizedWordGreedtStarAlgorithm

--- ontobuilder.matching/src/ac/technion/iem/ontobuilder/matching/algorithms/line1/misc/algorithms/TokenizedWordGreedyStarAlgorithm.java
+++ ontobuilder.matching/src/ac/technion/iem/ontobuilder/matching/algorithms/line1/misc/algorithms/TokenizedWordGreedyStarAlgorithm.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ac.technion.iem.ontobuilder.core.ontology.Term;
-import ac.technion.iem.ontobuilder.matching.algorithms.line1.misc.WordNetAlgorithm;
 /**
  * @author Arik Senderovic, Sapir Golan
  *
@@ -18,6 +17,9 @@ public class TokenizedWordGreedyStarAlgorithm implements TokenizedWordAlgorithm 
 		if (term != null) {
 			
 			String fullTermName = term.getName();
+			if (fullTermName != null) {
+				fullTermName = fullTermName.replaceAll("[^a-zA-Z]", "");
+			}
 			boolean continueTokenizing = true;
 			
 			TokenizedWordGreedyAlgorithm greedyAlgorithm = new TokenizedWordGreedyAlgorithm();
@@ -40,7 +42,6 @@ public class TokenizedWordGreedyStarAlgorithm implements TokenizedWordAlgorithm 
 					continueTokenizing=false;
 				}
 			}
-//			tokenizeTermsRecursive(term, result);
 		}
 		return result;
 	}
@@ -53,80 +54,3 @@ public class TokenizedWordGreedyStarAlgorithm implements TokenizedWordAlgorithm 
 	
 	}
 }
-//	public void tokenizeTermsRecursive(Term term, List<String> result)
-//	{ 
-//		int arg_pref = 0;
-//		int arg_suf = 0;
-//		
-//		
-//		
-//		
-//		String termName = term.getName();
-//		//term's name is empty
-//		if (termName == null || termName == "") {
-//			return;
-//		} else {
-//			//removing all non alphabetic characters from a String
-//			termName = termName.replaceAll("[^a-zA-Z]", "");
-//			int termLength = termName.length();
-//			String biggestPrefix = null;
-//			String biggestSuffix = null;
-//			for (int i = 1; i <= termLength; i++) {
-//				//if current prefix\suffix is a word in the English dictionary set it has the biggest prefix\suffix
-//				String currentPrefix = termName.substring(0, i);
-//				String currentSuffix = termName.substring(termLength-i,termLength);
-//				if (WordNetAlgorithm.isWordInDiction(currentPrefix) == true)
-//						{
-//							biggestPrefix = currentPrefix;
-//							arg_pref = i;
-//						}
-//				if (WordNetAlgorithm.isWordInDiction(currentSuffix) == true)
-//				{
-//					biggestSuffix = currentSuffix;
-//					arg_suf =termLength-i;
-//				}
-//				
-//			
-//			}
-//			
-//			this.calcResult(biggestPrefix, biggestSuffix, result);
-//			
-//			Term midTerm = new Term();
-//			//List<String> midResult = new ArrayList<String>();
-//			if (arg_suf-arg_pref>2)
-//			{
-//				//We assume that significant word is of two letters - otherwise it doesn't
-//				//contribue to semantics.
-//				midTerm.setName(termName.substring(arg_pref,arg_suf));
-//				tokenizeTermsRecursive(midTerm, result);		
-//								
-//			} 
-//		}
-//		return;
-//	}
-//	private void calcResult(String prefix,String suffix, List<String> result) {
-//		//List<String> result = new ArrayList<String>();
-//		//case Suffix and Prefix were found
-//		if (prefix == null && suffix == null) {
-//			result.add("");
-//		}
-//		//case found only Suffix
-//		else if (prefix == null && suffix != null) {
-//			result.add(suffix);
-//		}
-//		//case found only Prefix
-//		else if (prefix != null && suffix == null) {
-//			result.add(prefix);
-//		}
-//		//case prefix equals suffix (none of them can be null)
-//		else if ( suffix.equals(prefix) ) {
-//			result.add(prefix);
-//		}
-//		//case found both prefix and suffix
-//		else {
-//			result.add(suffix);
-//			result.add(prefix);
-//		}
-//		return;
-//	}
-//}
--- ontobuilder.matching/src/ac/technion/iem/ontobuilder/matching/algorithms/line1/misc/tests/TokenizedWordGreedyStarAlgorithmTest.java
+++ ontobuilder.matching/src/ac/technion/iem/ontobuilder/matching/algorithms/line1/misc/tests/TokenizedWordGreedyStarAlgorithmTest.java
@@ -29,7 +29,6 @@ public class TokenizedWordGreedyStarAlgorithmTest {
 
 	@Test
 	public void tokenizeTermsCompundValueTest() {
-
 		Term term = new Term();
 		term.setName("PurchaseCarBikeShipOrder");
 		List<String> result = classUndetTest.tokenizeTerms(term); 
@@ -39,7 +38,6 @@ public class TokenizedWordGreedyStarAlgorithmTest {
 		assertTrue("Result of algorithm doesn't contain 'Bike'", result.contains("Bike"));
 		assertTrue("Result of algorithm doesn't contain 'Ship'", result.contains("Ship"));
 		assertTrue("Result of algorithm doesn't contain 'Order'", result.contains("Order"));
-		
 	}
 	
 	@Test
@@ -59,17 +57,46 @@ public class TokenizedWordGreedyStarAlgorithmTest {
 		term.setName("Orders");
 		List<String> result = classUndetTest.tokenizeTerms(term); 
 		assertEquals("Algorithm didn't find all words", 1, result.size());
-		assertTrue("Result of algorithm doesn't contain 'Order'", result.contains("Order"));
+		assertTrue("Result of algorithm doesn't contain 'Orders'", result.contains("Orders"));
 	}
+	
+	@Test
 	public void tokenizeTermsMultiWordTest() {
-
 		Term term = new Term();
 		term.setName("purchaseimportantorder");
 		List<String> result = classUndetTest.tokenizeTerms(term); 
-		assertEquals("Algorithm didn't find all words", 5, result.size());
+		assertEquals("Algorithm didn't find all words", 3, result.size());
 		assertTrue("Result of algorithm doesn't contain 'purchase'", result.contains("purchase"));
 		assertTrue("Result of algorithm doesn't contain 'order'", result.contains("order"));
-		assertTrue("Result of algorithm doesn't contain 'order'", result.contains("important"));
+		assertTrue("Result of algorithm doesn't contain 'important'", result.contains("important"));
+	}
+	
+	@Test
+	public void tokenizeTermsMultiWordAndNoneWordsTest() {
+		Term term = new Term();
+		term.setName("77dogmovie");
+		List<String> result = classUndetTest.tokenizeTerms(term); 
+		assertEquals("Algorithm didn't find all words", 2, result.size());
+		assertTrue("Result of algorithm doesn't contain 'dog'", result.contains("dog"));
+		assertTrue("Result of algorithm doesn't contain 'movie'", result.contains("movie"));
+		
+		term.setName("Zdogmovie");
+		result = classUndetTest.tokenizeTerms(term); 
+		assertEquals("Algorithm didn't find all words", 2, result.size());
+		assertTrue("Result of algorithm doesn't contain 'dog'", result.contains("dog"));
+		assertTrue("Result of algorithm doesn't contain 'movie'", result.contains("movie"));
+		
+		term.setName("dogprince88");
+		result = classUndetTest.tokenizeTerms(term); 
+		assertEquals("Algorithm didn't find all words", 2, result.size());
+		assertTrue("Result of algorithm doesn't contain 'dog'", result.contains("dog"));
+		assertTrue("Result of algorithm doesn't contain 'prince'", result.contains("prince"));
+		
+		term.setName("dogprinceI");
+		result = classUndetTest.tokenizeTerms(term); 
+		assertEquals("Algorithm didn't find all words", 2, result.size());
+		assertTrue("Result of algorithm doesn't contain 'dog'", result.contains("dog"));
+		assertTrue("Result of algorithm doesn't contain 'prince'", result.contains("prince"));
 	}
 
 }
