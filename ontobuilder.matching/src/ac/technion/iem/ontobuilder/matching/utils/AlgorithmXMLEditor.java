e6d7111582be425a44e11687295e7df3ca62e8a6
tomers77 <tomers77@localhost>
2013-10-09 09:23:04 +0300

    more cleanup

--- ontobuilder.matching/src/ac/technion/iem/ontobuilder/matching/utils/AlgorithmXMLEditor.java
+++ ontobuilder.matching/src/ac/technion/iem/ontobuilder/matching/utils/AlgorithmXMLEditor.java
@@ -8,6 +8,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
